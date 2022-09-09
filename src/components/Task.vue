0d6a062947d92005908a1201f0016b3f73e5d8c6
Mikita Slabysh <slabysh2015@gmail.com>
2022-09-09 00:07:42 -0400

    First Commit

---
+++ src/components/Task.vue
@@ -0,0 +1,42 @@
+<template>
+    <!--you can also do @dblclick instead of @click for two clicks-->
+    <div @click="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{task.text}}
+        <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'Task-main',
+        props: {
+            task: Object,
+        },
+    }
+</script>
+
+<style scope>
+    .fas {
+        color: red;
+    }
+
+    .task {
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+
+    .task.reminder {
+        border-left: 5px solid green;
+    }
+
+    .task h3 {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
