689295a321f541dd08255360e2d724b1ab1ffd46
Mikita Slabysh <slabysh2015@gmail.com>
2025-06-20 20:02:47 -0400

    Started working on the login and sign up pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from 'vue-router'
 //.. in path to the file represents that the file is in the folder outside of this folder, so basically go out of the folder
 import About from '../views/About'
 import Home from '../views/Home'
+import Login from '../views/Login'
+import SignUp from '../views/SignUp'
 
 const routes = [
     {
@@ -13,7 +15,17 @@ const routes = [
         path: '/about',
         name: 'About',
         component: About
-    }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/signup',
+        name: 'Sign Up',
+        component: SignUp
+    },
 ]
 
 const router = createRouter({
---
+++ src/views/SignUp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="container">
+    <h1>Bucket List</h1>
+    <h2>Sign Up</h2>
+    <form action="">
+      <div class="form-group">
+        <label for="email">Email: </label>
+        <input type="email" id="userEmail">
+      </div>
+      <div class="form-group">
+        <label for="email">Password: </label>
+        <input type="password" id="userEmail">
+      </div>
+      <button id="submitButton">Sign Up</button>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SignUpView",
+  }
+</script>
+
+<style>
+
+.form-group label {
+  font-size: 20px;
+}
+
+.form-group input {
+  padding: 0.2vw 3vh;
+}
+
+#submitButton {
+  padding: 0.6vh 2.5vh;
+  background-color: blueviolet;
+  color: white;
+  cursor: pointer;
+  border-radius: 15px;
+}
+
+</style>
